0f6f4adf1d931fda3d5ab1fafba8a089752d69c2
Pierre Leon[user] <Leon.Pirre@HealthTrustWS.com>
2019-12-10 09:08:56 -0500

    - removed index stream as due to the slider keeping its current index internally

--- lib/viewmodels/memesviewmodel.dart
+++ lib/viewmodels/memesviewmodel.dart
@@ -1,33 +1,21 @@
 import 'dart:async';
+import 'dart:collection';
 
 import 'package:leonpierre_mememaker/models/mememodel.dart';
 import 'package:leonpierre_mememaker/services/MockApiService.dart';
 import 'package:leonpierre_mememaker/viewmodels/viewmodelprovider.dart';
-import 'package:queries/collections.dart';
 import 'package:rxdart/rxdart.dart';
 
 class MemesViewModel extends ViewModelBase {
   PublishSubject<List<Meme>> _memesSubject = PublishSubject<List<Meme>>();
   Stream<List<Meme>> get memes => _memesSubject.stream;
 
-  BehaviorSubject<int> _indexSubject = BehaviorSubject<int>.seeded(0);
-  Stream<int> get currentIndex => _indexSubject.stream;
-  void setCurrentIndex(int index) => _indexSubject.sink.add(index);
-
   final MockApiService service = MockApiService();
 
-  Timer _timer;
   List<Meme> _allMemes = List<Meme>();
-
-  MemesViewModel(){
-    //memesSubject.listen(_onMemesAdded);
-  }
+  Timer _timer;
 
   void initialize({bool filterExpression(Meme meme)}) async {
-    //have a service that pulls down data every x seconds and when the user does some action..?
-    // _timer = Timer.periodic(Duration(seconds: 5), (Timer timer) async {
-      
-    // });
     _onMemesAdded(await service.getMemesAsync(expression: filterExpression));
   }
 
@@ -42,12 +30,18 @@ class MemesViewModel extends ViewModelBase {
 
   void _onMemesAdded(List<Meme> memes) {
     _allMemes.addAll(memes);
-    _memesSubject.sink.add(_allMemes);
+    _memesSubject.sink.add(UnmodifiableListView<Meme>(_allMemes));
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 5), (Timer timer) async {
+
+    });
+    //Stream.periodic()
   }
 
   void dispose() {
     _timer?.cancel();
-    _indexSubject.close();
     _memesSubject.close();
   }
 }
