1ed8e5d5b3924d99ba1811e8b175775b4393b1b3
Leon Pierre <Leon.Pierre@HealthTrustWS.com>
2020-04-19 22:08:54 -0400

    - remvoed blocmanger and moved it to library - code clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-//import 'package:dev_libraries/services/analytics/facebookappeventservice.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:leonpierre_mememaker/app.dart';
-import 'package:leonpierre_mememaker/blocs/blocmanager.dart';
-import 'package:dev_libraries/services/logging/appspectorservice.dart';
-//import 'package:appspector/appspector.dart' as appSpector;
+import 'package:dev_libraries/dev_libraries.dart';
 
 
 void main() async
@@ -19,58 +15,3 @@ void main() async
   //debugPrintGestureArenaDiagnostics = true;
   runApp(App());
 }
-
-// enum LogLevel {
-//   Info,
-//   Log,
-//   Warn,
-//   Debug,
-//   Error,
-//   Fatal
-// }
-
-// abstract class LogService {
-//   void initialize();
-  
-//   void log(LogLevel level, Exception exception, String message);
-// }
-
-// class AppSpectorService extends LogService {
-//   final Map<String, String> configuration;
-//   AppSpectorService(this.configuration) : assert(configuration != null) {
-//     initialize();
-//   }
-
-//   @override
-//   void log(LogLevel level, Exception exception, String message) async {
-//     //the sdk picks up whatever is logged to the console
-//   }
-
-//   @override
-//   void initialize() async {
-//     if(!configuration.containsKey("androidApiKey"))
-//       throw Exception("Missing key from configuration androidApiKey");
-
-//     if(!configuration.containsKey("iosApiKey"))
-//       throw Exception("Missing key from configuration iosApiKey");
-
-//     var config = appSpector.Config();
-//     //..androidApiKey = configuration["androidApiKey"];
-//     config.iosApiKey = configuration["iosApiKey"];
-//     config.androidApiKey = configuration["androidApiKey"];
-//     appSpector.AppSpectorPlugin.run(config);
-//   }
-// }
-
-// abstract class AnalyticsService {
-//   activate();
-//   send(String event, Map<String, dynamic> parameters);
-// }
-
-// abstract class LoggableState {
-//   Map<String, dynamic> toLogState();
-// }
-
-// abstract class LoggableEvent {
-//   Map<String, dynamic> toLogState();
-// }
--- lib/views/screens/memeclusters.dart
+++ lib/views/screens/memeclusters.dart
@@ -3,10 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:leonpierre_mememaker/blocs/memeclusters/memeclusterbloc.dart';
 import 'package:leonpierre_mememaker/blocs/memeclusters/states.dart';
 import 'package:leonpierre_mememaker/views/memeclusters/views.dart';
-//import 'package:dev_libraries/bloc/adbloc.dart';
-//import 'package:dev_libraries/bloc/events.dart';
-//import 'package:dev_libraries/services/ads/adservice.dart';
-//import 'package:dev_libraries/services/ads/admobservice.dart';
 
 class MemeClustersPage extends StatefulWidget {
   @override
