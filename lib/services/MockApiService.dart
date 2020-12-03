dd88dc9d2f45e66a33f4b8ea5102a077dcfbcc9c
LeonPierre <lepierre08@gmail.com>
2020-12-03 13:03:07 -0500

    - cleaned up mockapiservice

--- lib/services/MockApiService.dart
+++ lib/services/MockApiService.dart
@@ -1,7 +1,12 @@
 import 'package:faker/faker.dart';
 import 'package:leonpierre_mememaker/models/mememodel.dart';
+import 'package:leonpierre_mememaker/repositories/entities/meme.dart';
+import 'package:queries/collections.dart';
+import 'package:leonpierre_mememaker/repositories/entities/memecluster.dart';
 
-class MockApiService {
+import 'memeclustersservice.dart';
+
+class MockApiService extends MemeClustersService{
   final mockApi = Faker();
   final DateTime now = DateTime.now();
 
@@ -36,4 +41,40 @@ class MockApiService {
             TextMeme("3", Uri(), DateTime.now().add(Duration(days: 5)),
                 now.toString()),
           ]);
+
+  @override
+  Future<IEnumerable<MemeClusterEntity>> byDateRangeAsync(DateTime start, DateTime end) {
+      List<MemeClusterEntity> clusters = List<MemeClusterEntity>();
+      List<MemeEntity> memes = List<MemeEntity>();
+
+      for(int i = 0; i < 5; i++)
+      {
+        
+        memes.add(MemeEntity(i.toString(), "https://via.placeholder.com/400x800?text=${faker.lorem.word()}", "ImageMeme"));
+        clusters.add(MemeClusterEntity(i.toString(), memes: Collection(memes)));
+      }
+
+      return Future.value(Collection(clusters));
+    }
+  
+    @override
+    Future<MemeClusterEntity> byIdAsync(String clusterId) {
+    // TODO: implement byIdAsync
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<IEnumerable<MemeClusterEntity>> byNewestAsync() {
+    List<MemeClusterEntity> clusters = List<MemeClusterEntity>();
+      List<MemeEntity> memes = List<MemeEntity>();
+
+      for(int i = 0; i < 5; i++)
+      {
+        
+        memes.add(MemeEntity(i.toString(), "https://via.placeholder.com/400x600?text=${faker.lorem.word()}", "ImageMeme"));
+        clusters.add(MemeClusterEntity(i.toString(), memes: Collection(memes)));
+      }
+
+      return Future.value(Collection(clusters));
+  }
 }
