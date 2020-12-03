297d80256787ab7c7817c23abba139bcce93fe1b
LeonPierre <lepierre08@gmail.com>
2020-12-03 12:54:12 -0500

    - update to bloc 6.0

--- lib/views/components/share.dart
+++ lib/views/components/share.dart
@@ -11,7 +11,7 @@ class ShareWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ShareBloc, ShareState>(
-        condition: (previousState, current) {
+        buildWhen: (previousState, current) {
           return previousState != current;
         },
         builder: (BuildContext context, ShareState state) {
