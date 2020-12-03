5c5cd9314f6cdf7fbb5a9f1336e03861fa4be540
LeonPierre <lepierre08@gmail.com>
2020-12-03 12:52:42 -0500

    - updated to bloc pattern and navigation bloc

--- lib/views/appcontainer.dart
+++ lib/views/appcontainer.dart
@@ -1,5 +1,7 @@
-import 'package:dev_libraries/bloc/app/app.dart';
-import 'package:dev_libraries/dev_libraries.dart';
+import 'package:dev_libraries/blocs/ads/ads.dart';
+import 'package:dev_libraries/blocs/configuration/configuration.dart';
+import 'package:dev_libraries/blocs/navigation_cubit.dart';
+import 'package:dev_libraries/models/navigationitem.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,16 +10,15 @@ import 'package:leonpierre_mememaker/blocs/favorites/events.dart';
 import 'package:leonpierre_mememaker/blocs/favorites/favoritesbloc.dart';
 import 'package:leonpierre_mememaker/blocs/memeclusters/bloc.dart';
 import 'package:leonpierre_mememaker/blocs/memeclusters/memeclusterbloc.dart';
-import 'package:leonpierre_mememaker/blocs/navigation.dart';
-import 'package:leonpierre_mememaker/models/navigationItem.dart';
 import 'package:leonpierre_mememaker/repositories/memeclusterrepository.dart';
+import 'package:leonpierre_mememaker/services/MockApiService.dart';
 import 'package:leonpierre_mememaker/views/screens/memeclusters.dart';
 
 import 'components/ads.dart';
 import 'screens/favorites.dart';
 
 class AppContainer extends StatefulWidget {
-  final NavigationBloc _navigationBloc;
+  final NavigationCubit _navigationBloc;
 
   AppContainer(this._navigationBloc);
 
@@ -32,75 +33,117 @@ class _AppContainerState extends State<AppContainer> {
   Widget build(BuildContext context) {
     _favoritesBloc = BlocProvider.of<FavoritesBloc>(context);
     _adBloc = BlocProvider.of<AdBloc>(context);
-    var configuration = BlocProvider.of<AppBloc>(context).configuration;
-    configuration["ad:width"] = MediaQuery.of(context).size.width.round();
-    configuration["ad:height"] = (MediaQuery.of(context).size.height * .1).round();
+    
+    return BlocBuilder<ConfigurationBloc, ConfigurationState>(
+        builder: (BuildContext context, ConfigurationState state) {
+      if (state is ConfigurationUnitializedState)
+        return CircularProgressIndicator();
+      else if (state is ConfigurationErrorState)
+        return Center(child: Text(state.message));
+      else if (state is ConfigurationInitializedState)
+        return BlocBuilder<NavigationCubit, NavigationItemModel>(
+            cubit: widget._navigationBloc,
+            builder: (BuildContext context, NavigationItemModel pageModel) {
+              state.configuration["ad:width"] = MediaQuery.of(context).size.width.round();
+              state.configuration["ad:height"] = (MediaQuery.of(context).size.height * .1).round();
 
-   return Scaffold(
-      appBar: AppBar(title: Text(configuration["title"])),
-      body: StreamBuilder(
-            stream: widget._navigationBloc.pages,
-            initialData: widget._navigationBloc.navigation.value,
-            builder: (BuildContext context, AsyncSnapshot<NavigationItemModel> snapshot) {
-              return Column(
-                children: <Widget>[
-                   Expanded(child: _buildPage(snapshot.data.item), flex: 10),
-                   Expanded(child: AdsWidget(_adBloc, configuration), flex: 1)
-            ]);
-        }),
-      
-      //TODO implement this animation https://pub.dev/packages/curved_navigation_bar
-      bottomNavigationBar: StreamBuilder(builder:
-          (BuildContext context, AsyncSnapshot<NavigationItem> snapshot) {
-        return BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.flare),
-                title: Text('Home/What' 's hot'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.save),
-                title: Text('Favorites'),
-              ),
-            ],
-            currentIndex: widget._navigationBloc.navigation.value.index,
-            selectedItemColor: Theme.of(context).primaryColor,
-            onTap: (index) {
-              widget._navigationBloc.toPage(index);
-              //notify ui of state change
-              setState(() {});
+              return Scaffold(
+                  appBar: AppBar(title: Text(state.configuration["title"])),
+                  body: Column(children: <Widget>[
+                    Expanded(child: _buildPage(pageModel), flex: 10),
+                    Expanded(child: AdsWidget(_adBloc, state.configuration), flex: 1)
+                  ]),
+
+                  //TODO implement this animation https://pub.dev/packages/curved_navigation_bar
+                  bottomNavigationBar: StreamBuilder(builder:
+                      (BuildContext context, AsyncSnapshot<NavigationItem> snapshot) {
+                    return BottomNavigationBar(
+                        items: const <BottomNavigationBarItem>[
+                          BottomNavigationBarItem(
+                            icon: Icon(Icons.flare),
+                            label: 'Home/What' 's hot'
+                          ),
+                          BottomNavigationBarItem(
+                            icon: Icon(Icons.save),
+                            label: 'Favorites'
+                          ),
+                        ],
+                        currentIndex: pageModel.index,
+                        selectedItemColor: Theme.of(context).primaryColor,
+                        onTap: (index) {
+                          widget._navigationBloc.toPage(index);
+                          //notify ui of state change
+                          setState(() {});
+                        });
+                  }));
             });
-      }));
+
+      //   return Scaffold(
+      //     appBar: AppBar(title: Text(state.configuration["title"])),
+      //     body: BlocBuilder<NavigationCubit, NavigationItemModel>(
+      //       cubit: widget._navigationBloc,
+      //       builder: (BuildContext context, NavigationItemModel pageModel) {
+      //         return Column(
+      //           children: <Widget>[
+      //              Expanded(child: _buildPage(pageModel.item), flex: 10),
+      //              Expanded(child: AdsWidget(_adBloc, state.configuration), flex: 1)
+      //       ]);
+      //       }
+      // ),
+
+      // //TODO implement this animation https://pub.dev/packages/curved_navigation_bar
+      // bottomNavigationBar: StreamBuilder(builder:
+      //     (BuildContext context, AsyncSnapshot<NavigationItem> snapshot) {
+      //   return BottomNavigationBar(
+      //       items: const <BottomNavigationBarItem>[
+      //         BottomNavigationBarItem(
+      //           icon: Icon(Icons.flare),
+      //           title: Text('Home/What' 's hot'),
+      //         ),
+      //         BottomNavigationBarItem(
+      //           icon: Icon(Icons.save),
+      //           title: Text('Favorites'),
+      //         ),
+      //       ],
+      //       currentIndex: widget._navigationBloc.navigation.value.index,
+      //       selectedItemColor: Theme.of(context).primaryColor,
+      //       onTap: (index) {
+      //         widget._navigationBloc.toPage(index);
+      //         //notify ui of state change
+      //         setState(() {});
+      //       });
+      // }));
+
+      return Container();
+    });
   }
-  
-  Widget _buildPage(NavigationItem navigationItem) {
-    switch (navigationItem) {
-      case NavigationItem.HOME:
+
+  Widget _buildPage(NavigationItemModel navigationItem) {
+    switch (navigationItem.index) {
+      case 0:
         return _buildHomePage();
-      case NavigationItem.FAVORITES:
+      case 1:
         return _buildFavoritesPage();
-      case NavigationItem.SEARCH:
+      case 2:
       default:
         return null;
     }
   }
 
   Widget _buildHomePage() {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<MemeClusterBloc>(
-          create: (BuildContext context) => MemeClusterBloc(MemeClusterRepository(), _favoritesBloc)
-          ..add(MemeClusterEvent(MemeClusterEventId.LoadMemeClusters))
-          ),
-          BlocProvider<ShareBloc>(create: (BuildContext context) => ShareBloc())
-        ],
-        child: MemeClustersPage()
-      );
+    return MultiBlocProvider(providers: [
+      BlocProvider<MemeClusterBloc>(
+          create: (BuildContext context) =>
+              MemeClusterBloc(MockApiService(), _favoritesBloc)
+                ..add(MemeClusterEvent(MemeClusterEventId.LoadMemeClusters))),
+      BlocProvider<ShareBloc>(create: (BuildContext context) => ShareBloc())
+    ], child: MemeClustersPage());
   }
 
   Widget _buildFavoritesPage() {
     return BlocProvider.value(
-      value: _favoritesBloc..add(FavoritesLoadEvent(FavoritesEventId.FavoritesUnitialized)),
-      child: FavoritesPage());
+        value: _favoritesBloc
+          ..add(FavoritesLoadEvent(FavoritesEventId.FavoritesUnitialized)),
+        child: FavoritesPage());
   }
 }
