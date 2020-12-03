b98d9268aae2f01521c230aa6127429a34b025ba
LeonPierre <lepierre08@gmail.com>
2020-12-03 12:50:02 -0500

    - updated model

--- lib/repositories/entities/contentbase.dart
+++ lib/repositories/entities/contentbase.dart
@@ -2,13 +2,16 @@ import 'package:equatable/equatable.dart';
 
 class ContentBaseEntity extends Equatable {
   final String id;
+  final String name; 
+  final String description;
   final String path;
   final DateTime dateLiked;
 
-  const ContentBaseEntity(this.id, { this.path, this.dateLiked });
+  const ContentBaseEntity(this.id, { this.name, this.description, this.path, this.dateLiked });
 
   factory ContentBaseEntity.fromJson(Map<String, dynamic> json) {
-    return ContentBaseEntity(json['id'], path: json['path'], dateLiked: json['dateLiked'] == null ? null : DateTime.parse(json['dateLiked']));
+    return ContentBaseEntity(json['id'], path: json['path'], name: json['name'], description: json['description'],
+    dateLiked: json['dateLiked'] == null ? null : DateTime.parse(json['dateLiked']));
   }
   
   @override
