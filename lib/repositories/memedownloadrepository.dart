4e7995aaa990b5a2954c32fb1904c94af7471ad8
LeonPierre <lepierre08@gmail.com>
2020-12-04 14:07:07 -0500

    - added download repository

---
+++ lib/repositories/memedownloadrepository.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:leonpierre_mememaker/repositories/entities/meme.dart';
+import 'package:leonpierre_mememaker/services/memedownloadservice.dart';
+
+class MemeDownloadRepository extends MemeDownloadService {
+  //final String baseUrl = "http://108.175.11.170/api";
+  final String baseUrl = "https://leonipierre.ngrok.io/api";
+
+  @override
+  Future<File> downloadAsync(String url) {
+    // TODO: implement downloadAsync
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> getContentUrlAsync(String url) {
+    // TODO: implement getContentUrlAsync
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<MemeEntity> getMemeByUrlAsync(String url) async => await http
+          .post('$baseUrl/meme',
+              headers: <String, String>{'Content-Type': 'application/json; charset=UTF-8'},
+              body: jsonEncode([url]))
+          .then((response) {
+        if (response.statusCode != 200) throw Exception("");
+
+        return MemeEntity.fromJson(json.decode(response.body));
+      });
+
+  Future<http.Response> _getResponse(String url) async =>
+      await http.get(url).timeout(Duration(minutes: 10));
+}
